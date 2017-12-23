b5379cc32d69efbfa63a50d3f8f333ae7374dc03
nguyenvannam19942802 <nguyenvannam19942802@gmail.com>
2017-12-23 14:43:49 +0700

    # khởi tạo ứng dụng đọc list contact và gửi SMS

---
+++ app/src/main/java/androidapp/focus/hoangnam/application_read_contact_sentsms/MainActivity.java
@@ -0,0 +1,13 @@
+package androidapp.focus.hoangnam.application_read_contact_sentsms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
